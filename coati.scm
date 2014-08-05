88b572a74607eccce0d70c29080bba935c302376
pluizer <pluizer@freeshell.de>
2014-08-05 17:26:28 +0200

    better trans functions

--- coati.scm
+++ coati.scm
@@ -257,17 +257,29 @@ SOFTWARE.
      (begin (%camera:push position scale rotation) form ... (%camera:pop)))))
 
 
-;; Transformation
-(define (trans source-rect #!key
-	       (dest-rect source-rect)
-	       (origin (vect:create 0 0))
-	       (rotation 0)
-	       (flip-h -1)
-	       (flip-v -1))
-  (flatten (f32vector->list source-rect)
-	   (f32vector->list dest-rect)
-	   (f32vector->list origin)
-	   rotation flip-h flip-v))
+;; Trans
+(define (trans:create source-rect #!optional
+		      (dest-rect source-rect)
+		      (origin (vect:create 0 0))
+		      (rotation 0)
+		      flip-h
+		      flip-v)
+  (array->transformation
+   (apply f32vector (flatten (f32vector->list source-rect)
+			     (f32vector->list dest-rect)
+			     (f32vector->list origin)
+			     rotation (if flip-h 1 -1) (if flip-v 1 -1)))))
+
+(define (trans:make source-rect #!key
+		    (dest-rect source-rect)
+		    (origin (vect:create 0 0))
+		    (rotation 0)
+		    (flip-h #f)
+		    (flip-v #f))
+  (trans:create source-rect dest-rect origin rotation 
+		flip-h
+		flip-v))
+
 ;; Input
 (bind-coati quitting? bool ())
 (bind-coati key:pressed? bool (key))
