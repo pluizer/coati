ed72f71ef2588a9a6b95de257d840d6106126fe2
pluizer <pluizer@freeshell.de>
2014-09-16 18:51:27 +0200

    shuffled code a bit

--- src/scheme/batch.scm
+++ src/scheme/batch.scm
@@ -7,23 +7,3 @@
 (bind-coati batch:change void (batch unsigned-int transformation))
 (bind-coati batch:render void (batch texture))
 (bind-coati batch:size unsigned-int (batch))
-
-(define-mystruct animation
-  (rects
-   (interval value: (/ 1000 20))
-   (%epoch   value: (current-milliseconds))))
-
-(define (animation:new-frame? ani)
-  (> (- (current-milliseconds)
-	(animation:%epoch ani))
-     (animation:interval ani)))
-
-(define (animation:rect ani)
-  (when (animation:new-frame? ani)
-	(pop-cycle (animation:rects ani))
-	(set! (animation:%epoch ani) (current-milliseconds)))
-  (car (animation:rects ani)))
-
-(define-general :rect animation? animation:rect)
-
-(define-general :new-frame animation? animation:new-frame?)
--- src/scheme/sprite.scm
+++ src/scheme/sprite.scm
@@ -2,3 +2,23 @@
   (rect))
 
 (define-general :rect sprite? sprite:rect)
+
+(define-mystruct animation
+  (rects
+   (interval value: (/ 1000 20))
+   (%epoch   value: (current-milliseconds))))
+
+(define (animation:new-frame? ani)
+  (> (- (current-milliseconds)
+	(animation:%epoch ani))
+     (animation:interval ani)))
+
+(define (animation:rect ani)
+  (when (animation:new-frame? ani)
+	(pop-cycle (animation:rects ani))
+	(set! (animation:%epoch ani) (current-milliseconds)))
+  (car (animation:rects ani)))
+
+(define-general :rect animation? animation:rect)
+
+(define-general :new-frame animation? animation:new-frame?)
